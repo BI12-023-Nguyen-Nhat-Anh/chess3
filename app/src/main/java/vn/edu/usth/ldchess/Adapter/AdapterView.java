d3a414a388d51fb330103515bdc54a4416753e14
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-09-19 14:14:32 +0700

    Pageview for main

---
+++ app/src/main/java/vn/edu/usth/ldchess/Adapter/AdapterView.java
@@ -0,0 +1,33 @@
+package vn.edu.usth.ldchess.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import vn.edu.usth.ldchess.Intro;
+import vn.edu.usth.ldchess.List_Lesson;
+
+public class AdapterView extends FragmentStateAdapter {
+    public AdapterView(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return true;
+            case 1:
+                return new Intro();
+            case 2:
+                return new List_Lesson();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
