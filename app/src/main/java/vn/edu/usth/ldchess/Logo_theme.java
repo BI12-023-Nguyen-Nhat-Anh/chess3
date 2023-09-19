f1f9a7f06a4fa8071ab8a319c9c496499c72c31d
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-09-19 14:41:52 +0700

    Logo

--- app/src/main/java/vn/edu/usth/ldchess/Logo_theme.java
+++ app/src/main/java/vn/edu/usth/ldchess/Logo_theme.java
@@ -2,7 +2,9 @@ package vn.edu.usth.ldchess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Logo_theme extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Logo_theme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_theme);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Logo_theme.this, MainActivity.class));
+            }
+        },3000);
     }
+
 }
