e3588b8f9760bf48c65747129c53391400c10c82
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-09-19 15:43:12 +0700

    Main file

--- app/src/main/java/vn/edu/usth/ldchess/MainActivity.java
+++ app/src/main/java/vn/edu/usth/ldchess/MainActivity.java
@@ -3,17 +3,72 @@ package vn.edu.usth.ldchess;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import vn.edu.usth.ldchess.Adapter.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="Main Activity";
+    ViewPager2 viewpager ;
+    AdapterView adapterView;
+    BottomNavigationView nav_bottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: Sucess");
+
+        viewpager = findViewById(R.id.viewpager);
+        nav_bottom = findViewById(R.id.nav_bottom);
+
+        adapterView = new AdapterView(this);
+        viewpager.setAdapter(adapterView);
+        viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        nav_bottom.setSelectedItemId(R.id.home);
+                        break;
+                    case 1:
+                        nav_bottom.setSelectedItemId(R.id.lesson);
+                        break;
+                    case 2:
+                        nav_bottom.setSelectedItemId(R.id.profile);
+                        break;
+                }
+                super.onPageSelected(position);
+            }
+        });
+        nav_bottom.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();//                    case R.id.home_nav:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.home_nav);
+//                        break;
+                if (itemId == R.id.lesson) {
+                    viewpager.setCurrentItem(1);
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    viewpager.setCurrentItem(2);
+                    return true;
+                }
+                else if (itemId == R.id.home) {
+                    viewpager.setCurrentItem(0);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
