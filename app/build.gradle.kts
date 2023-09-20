df86eae889bcd1ac1be13b4fd747a02d356d6085
nguyennhatanh2103@gmail.com <anhnn.bi12-023@st.usth.edu.vn>
2023-09-20 09:40:55 +0700

    Fix library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,5 +36,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("android.cardview:cardview:1.0.0")
+    releaseImplementation("android.cardview:cardview:1.0.0")
 }
