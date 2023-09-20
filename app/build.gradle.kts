982a7476ee8e3bc48a33ef7b20cf3434252489b0
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-09-20 10:32:37 +0700

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-<<<<<<< HEAD
 
-=======
+
+
     releaseImplementation("android.cardview:cardview:1.0.0")
->>>>>>> a36f5e3b9522e2015539ed21b5a938a1c795e81d
+
 }
