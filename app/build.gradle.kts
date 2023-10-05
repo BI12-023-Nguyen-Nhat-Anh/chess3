c3162200de0eff00d61923032b11577691a98ec9
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-10-05 11:41:24 +0700

    Add library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.squareup.picasso:picasso:2.8")
     releaseImplementation("android.cardview:cardview:1.0.0")
 
 }
