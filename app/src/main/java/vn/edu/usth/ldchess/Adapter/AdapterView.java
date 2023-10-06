9b0401efbe29ebfd50783ac4fa0d7686a68545ab
nguyennhatanh2103@gmail.com <anhnn.bi12-023@st.usth.edu.vn>
2023-10-06 15:55:50 +0700

    test chess

--- app/src/main/java/vn/edu/usth/ldchess/Adapter/AdapterView.java
+++ app/src/main/java/vn/edu/usth/ldchess/Adapter/AdapterView.java
@@ -8,6 +8,8 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import vn.edu.usth.ldchess.HeaderProfileFragment;
 import vn.edu.usth.ldchess.Home_Explore_Content;
 import vn.edu.usth.ldchess.List_Chess;
+import vn.edu.usth.ldchess.PlayChess;
+import vn.edu.usth.ldchess.PlayChessFragment;
 
 public class AdapterView extends FragmentStateAdapter {
     public AdapterView(@NonNull FragmentActivity fragmentActivity) {
@@ -23,7 +25,7 @@ public class AdapterView extends FragmentStateAdapter {
             case 1:
                 return new List_Chess();
             case 2:
-                return new PlayChess();
+                return new PlayChessFragment();
             case 3:
                 return new HeaderProfileFragment();
         }
